b6eb0a0c927d9e54260996d06cd7e08980a40ce2
Niki Jiandani <niki.jiandani@shopify.com>
2022-06-29 11:54:24 -0400

    Remove script source command

--- install.sh
+++ install.sh
@@ -4,5 +4,3 @@
 # updated, the terminal will pick up the new version on reload without having to run
 # install again. This will overwrite any existing .zshrc.
 ln -vsfn ~/dotfiles/.zshrc ~/.zshrc
-
-source ~/dotfiles/personal/install.sh
