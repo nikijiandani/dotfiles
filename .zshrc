d0df194f2e615c29c5a13ba387544efbc9e7eb8f
Niki Jiandani <niki.jiandani@shopify.com>
2022-06-29 11:47:21 -0400

    Add aliases and install script

--- .zshrc
+++ .zshrc
@@ -1,2 +1,9 @@
 
 [ -f /opt/dev/dev.sh ] && source /opt/dev/dev.sh
+
+[[ -f /opt/dev/sh/chruby/chruby.sh ]] && type chruby >/dev/null 2>&1 || chruby () { source /opt/dev/sh/chruby/chruby.sh; chruby "$@"; }
+
+[[ -x /opt/homebrew/bin/brew ]] && eval $(/opt/homebrew/bin/brew shellenv)
+
+alias gs='git status'
+alias gb='git branch'
---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env zsh
+
+# Symlink this repo's .zshrc to ~/.zshrc. Using a symlink ensures that when the repo is
+# updated, the terminal will pick up the new version on reload without having to run
+# install again. This will overwrite any existing .zshrc.
+ln -vsfn ~/dotfiles/.zshrc ~/.zshrc
+
+source ~/dotfiles/personal/install.sh
